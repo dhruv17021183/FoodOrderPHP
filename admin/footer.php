6396a3fa05e353f93cdfd43dd1a7961ca2599a4b
Dhruv <dsuthar1183@gmail.com>
2021-06-12 19:15:54 +0530

    initial commit

---
+++ admin/footer.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<div class="footer">
+ 	<div class="wrapper">
+ 		<p class="text-center">2021 all rights reserved,Developed By -<a href="#">Dhruv Suthar</a></p>
+ 	</div>
+ </div>
+</body>
+</html>
---
+++ admin/manu.php
@@ -0,0 +1,26 @@
+<?php include ('../config/constants.php'); ?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>FoodOrder-HomePage</title>
+	<link rel="stylesheet" type="text/css" href="../css/admin.css">
+
+
+<body>
+ <div class="menu">
+ 	<div class="wrapper">
+ 	  <ul>
+		 		<li><a href="index.php">Home</a></li>
+		 		<li><a href="manage-admin.php">Admin</a></li>
+		 		<li><a href="manage-catagory.php">Category</a></li>
+		 		<li><a href="manage-food.php">Food</a></li>
+		 		<li><a href="manage-order.php">Order</a></li>
+		 		<li><a href="Log-Out.php">Log-Out</a></li>
+ 	  </ul>
+ 	</div>
+ </div>
+ </body>
+</head>
+</html>
